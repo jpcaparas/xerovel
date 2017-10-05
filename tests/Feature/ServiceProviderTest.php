103a2f1e749d98dd0dfeb5194b87c7e8bfd56cf8
JP Caparas <jp@jpcaparas.com>
2017-10-06 09:12:29 +1300

    Bootstrap tests

---
+++ tests/Feature/ServiceProviderTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace JPCaparas\Tests\Xerovel\Feature;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class MetaTest
+ *
+ * @package JPCaparas\Tests\Xerovel\Feature
+ */
+class ServiceProviderTest extends TestCase
+{
+    public function testTrueIsTrue()
+    {
+        $true = true;
+        $this->assertEquals(true, $true);
+    }
+}
